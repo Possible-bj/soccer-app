a3638ea8ba2c24b77d338836ff9218dd6de9736c
Possible <ahunanyabenjamin@gmail.com>
2021-01-12 09:42:37 +0100

    Login Expansion Continues

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -6,7 +6,7 @@ const auth = async (req, res, next) => {
     try {
         // const token = req.header('Authorization').replace('Bearer ', '')
         const token = req.query.token
-        const decoded = jwt.verify(token, 'suprememoderator')
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
         const admin = await Admin.findOne({ _id: decoded._id, 'tokens.token': token})
 
         if (!admin) {
--- src/models/admin.js
+++ src/models/admin.js
@@ -29,7 +29,7 @@ const adminSchema = new mongoose.Schema({
 
 adminSchema.methods.generateAuthToken = async function () {
     const admin = this
-    const token = jwt.sign({_id: admin._id.toString()}, 'suprememoderator')
+    const token = jwt.sign({_id: admin._id.toString()}, process.env.JWT_SECRET)
     admin.tokens = admin.tokens.concat({ token })
     await admin.save()
     return token
