e8d5f934f6c6311c595cdb5bc48f3ca6a9880a5d
Possible <ahunanyabenjamin@gmail.com>
2023-09-24 15:24:52 +0100

    Update cors

--- src/index.js
+++ src/index.js
@@ -1,47 +1,49 @@
-const express = require('express')
-require('./db/mongoose')
-const path = require('path')
+const express = require("express");
+require("./db/mongoose");
+const path = require("path");
 // const cookie = require('cookie-parser')
-const hbs = require('hbs')
-const metadataRouter = require('./routers/metadata')
-const pageRouter = require('./routers/pages')
-const leagueRouter = require('./routers/league')
-const leagueResultRouter = require('./routers/leagueResults')
-const gsRouter = require('./routers/sclGS')
-const qfRouter = require('./routers/sclQF')
-const sfRouter = require('./routers/sclSF')
-const finRouter = require('./routers/sclFIN')
-const authRouter = require('./routers/admin')
-const teamRouter = require('./routers/team')
+const hbs = require("hbs");
+const cors = require("cors");
+const metadataRouter = require("./routers/metadata");
+const pageRouter = require("./routers/pages");
+const leagueRouter = require("./routers/league");
+const leagueResultRouter = require("./routers/leagueResults");
+const gsRouter = require("./routers/sclGS");
+const qfRouter = require("./routers/sclQF");
+const sfRouter = require("./routers/sclSF");
+const finRouter = require("./routers/sclFIN");
+const authRouter = require("./routers/admin");
+const teamRouter = require("./routers/team");
 
-const app = express()  
-const port = process.env.PORT
-app.use(metadataRouter)
-app.use(pageRouter)
-app.use(leagueRouter)
-app.use(leagueResultRouter)
-app.use(gsRouter)
-app.use(qfRouter)
-app.use(sfRouter)
-app.use(finRouter)
-app.use(authRouter)
-app.use(teamRouter)
+const app = express();
+const port = process.env.PORT;
+app.use(cors());
+app.use(metadataRouter);
+app.use(pageRouter);
+app.use(leagueRouter);
+app.use(leagueResultRouter);
+app.use(gsRouter);
+app.use(qfRouter);
+app.use(sfRouter);
+app.use(finRouter);
+app.use(authRouter);
+app.use(teamRouter);
 
-const publicPath = path.join(__dirname, '../public')
-const viewsPath = path.join(__dirname, '../templates/views')
-const partialsPath = path.join(__dirname, '../templates/partials')
+const publicPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
 
-app.set('view engine', 'hbs')
-app.set('views', viewsPath)
-hbs.registerPartials(partialsPath)
+app.set("view engine", "hbs");
+app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
 
-app.use(express.static(publicPath))
-app.get('/*', (req, res) => {
-  res.render('404', {
-      title: 'Page Not Found!',
-      name: 'Benjamin Possible'
-  })
-})
+app.use(express.static(publicPath));
+app.get("/*", (req, res) => {
+  res.render("404", {
+    title: "Page Not Found!",
+    name: "Benjamin Possible",
+  });
+});
 app.listen(port, () => {
-    console.log(`Server started at port ${port}`)
-})
+  console.log(`Server started at port ${port}`);
+});
